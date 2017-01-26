b0bd49e47cb38b018df3f9ab693a77d29a4a9fc8
MinhNguyen <hatuongminh96@yahoo.com>
2017-01-25 17:04:11 -0700

    rm raise Error

--- NoGo1/board.py
+++ NoGo1/board.py
@@ -415,9 +415,9 @@ class GoBoard(object):
 
         if self.board[point] != EMPTY:
             c=self._point_to_coord(point)
-            msg = "Row and Column: %d %d is already filled with a %s stone"%(c[0],c[1],GoBoardUtil.int_to_color(color))
-            #return False,msg
-            raise ValueError("(occupied)")
+            #msg = "Row and Column: %d %d is already filled with a %s stone"%(c[0],c[1],GoBoardUtil.int_to_color(color))
+            msg = "(occupied)"
+            return False,msg
         if point == self.ko_constraint:
             msg ="KO move is not permitted!"
             return False , msg
